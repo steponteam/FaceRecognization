f2cb38bb25b4ac3c04b5d663c666ca39ac6ae02c
000010 <wuxinghua@stepon.tech>
2017-12-25 13:38:56 +0800

    demo's layout bug fixed

--- Stepon.FaceRecognization/FaceDemo/Main.Designer.cs
+++ Stepon.FaceRecognization/FaceDemo/Main.Designer.cs
@@ -33,12 +33,16 @@
             this.userIdentity = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.videoImage = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.videoImage)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // realtimeBtn
             // 
-            this.realtimeBtn.Location = new System.Drawing.Point(448, 1086);
+            this.realtimeBtn.Location = new System.Drawing.Point(452, 8);
             this.realtimeBtn.Name = "realtimeBtn";
             this.realtimeBtn.Size = new System.Drawing.Size(169, 39);
             this.realtimeBtn.TabIndex = 1;
@@ -48,7 +52,7 @@
             // 
             // pictureBtn
             // 
-            this.pictureBtn.Location = new System.Drawing.Point(623, 1086);
+            this.pictureBtn.Location = new System.Drawing.Point(627, 8);
             this.pictureBtn.Name = "pictureBtn";
             this.pictureBtn.Size = new System.Drawing.Size(169, 39);
             this.pictureBtn.TabIndex = 1;
@@ -58,7 +62,7 @@
             // 
             // userIdentity
             // 
-            this.userIdentity.Location = new System.Drawing.Point(98, 1091);
+            this.userIdentity.Location = new System.Drawing.Point(102, 13);
             this.userIdentity.Name = "userIdentity";
             this.userIdentity.Size = new System.Drawing.Size(338, 28);
             this.userIdentity.TabIndex = 2;
@@ -66,7 +70,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 1096);
+            this.label1.Location = new System.Drawing.Point(16, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(80, 18);
             this.label1.TabIndex = 3;
@@ -74,23 +78,49 @@
             // 
             // videoImage
             // 
-            this.videoImage.Location = new System.Drawing.Point(15, 12);
+            this.videoImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoImage.Location = new System.Drawing.Point(3, 3);
             this.videoImage.Name = "videoImage";
-            this.videoImage.Size = new System.Drawing.Size(1380, 1068);
+            this.videoImage.Size = new System.Drawing.Size(1118, 1092);
             this.videoImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.videoImage.TabIndex = 4;
             this.videoImage.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.videoImage, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 64F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1124, 1162);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.realtimeBtn);
+            this.panel1.Controls.Add(this.userIdentity);
+            this.panel1.Controls.Add(this.pictureBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 1101);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1118, 58);
+            this.panel1.TabIndex = 5;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1407, 1137);
-            this.Controls.Add(this.videoImage);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.userIdentity);
-            this.Controls.Add(this.pictureBtn);
-            this.Controls.Add(this.realtimeBtn);
+            this.ClientSize = new System.Drawing.Size(1148, 1186);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -98,8 +128,10 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "人脸识别测试程序";
             ((System.ComponentModel.ISupportInitialize)(this.videoImage)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -109,6 +141,8 @@
         private System.Windows.Forms.TextBox userIdentity;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox videoImage;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
